a1bd242e7fb7ae67b67e85189ae4fe1e724aee59
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-02 23:51:38 +0100

    model: deleted unnecessary comments

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module FareFinderApi
     # Enable CORS for development environment
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:5173' # Vite's default dev server URL
+        origins 'http://localhost:5173'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
