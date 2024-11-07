84c0f41b917d7fe676fe66a6e0e5f97b6e001064
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-07 12:33:52 +0100

    models: created migrations for adding column connections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_07_075910) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_07_094119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_07_075910) do
     t.bigint "line_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "connections"
     t.index ["line_id"], name: "index_lrt2s_on_line_id"
   end
 
@@ -46,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_07_075910) do
     t.bigint "line_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "connections"
     t.index ["line_id"], name: "index_mrt3s_on_line_id"
   end
 
